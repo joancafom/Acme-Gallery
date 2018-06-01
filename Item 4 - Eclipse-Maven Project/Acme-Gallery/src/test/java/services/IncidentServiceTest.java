31ec3a27139e772a559494773abde35692b37cd5
josembell <josembell97@gmail.com>
2018-06-01 02:10:15 +0200

    🍩 Belli - [UC-025], [UC-030]

--- Item 4 - Eclipse-Maven Project/Acme-Gallery/src/test/java/services/IncidentServiceTest.java
+++ Item 4 - Eclipse-Maven Project/Acme-Gallery/src/test/java/services/IncidentServiceTest.java
@@ -42,7 +42,7 @@ public class IncidentServiceTest extends AbstractTest {
 	/*
 	 * [UC-024] - Director check an Incident
 	 * 1. Log in as director
-	 * 2. Check my incidents
+	 * 2. List my incidents
 	 * 3. Mark an incident as checked
 	 * 
 	 * REQ: 19, 29.11
@@ -106,6 +106,79 @@ public class IncidentServiceTest extends AbstractTest {
 			final Director director = this.directorService.findByUserAccount(LoginService.getPrincipal());
 			final Collection<Incident> myIncidents = this.incidentService.getByDirector(director);
 			Assert.isTrue(myIncidents.contains(incident));
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.unauthenticate();
+		this.checkExceptions(expected, caught);
+
+	}
+
+	/*
+	 * [UC-025] - Director delete an Incident
+	 * 1. Log in as director
+	 * 2. List my incidents
+	 * 3. Delete an incident
+	 * 
+	 * REQ: 19, 29.11
+	 * 
+	 * v1.0 - josembell
+	 */
+	@Test
+	public void driverDeleteIncident() {
+
+		final Object testingData[][] = {
+			{
+				/* + 1. Un director elimina un indicent */
+				"director1", "incident2", null
+			}, {
+				/* - 2. Un usuario no identificado elimina un incident */
+				null, "incident2", IllegalArgumentException.class
+			}, {
+				/* - 3. Un usuario que no es director elimina un incident */
+				"visitor1", "incident2", IllegalArgumentException.class
+			}, {
+				/* - 4. Un director elimina un incident null */
+				"director1", null, IllegalArgumentException.class
+			}, {
+				/* - 5. Un director elimina un incident que no es suyo */
+				"director1", "incident15", IllegalArgumentException.class
+			}, {
+				/* - 6. Un director elimina un incident que ya estaba checkeado como checkeado */
+				"director1", "incident1", IllegalArgumentException.class
+			}
+
+		};
+
+		for (int i = 0; i < testingData.length; i++) {
+			Incident incident = null;
+			if (testingData[i][1] != null)
+				incident = this.incidentService.findOne(this.getEntityId((String) testingData[i][1]));
+			this.startTransaction();
+			this.templateDeleteIncident((String) testingData[i][0], incident, (Class<?>) testingData[i][2]);
+			this.rollbackTransaction();
+			this.entityManager.clear();
+		}
+
+	}
+
+	/* v1.0 - josembell */
+	protected void templateDeleteIncident(final String username, final Incident incident, final Class<?> expected) {
+		Class<?> caught = null;
+		/* 1. authenticate */
+		this.authenticate(username);
+
+		try {
+			/* 3. mark an incident as checked */
+			this.incidentService.delete(incident);
+
+			/* 2. list my incidents */
+
+			final Director director = this.directorService.findByUserAccount(LoginService.getPrincipal());
+			final Collection<Incident> myIncidents = this.incidentService.getByDirector(director);
+			Assert.isTrue(!myIncidents.contains(incident));
 
 		} catch (final Throwable oops) {
 			caught = oops.getClass();
--- Item 4 - Eclipse-Maven Project/Acme-Gallery/src/test/java/services/RoomServiceTest.java
+++ Item 4 - Eclipse-Maven Project/Acme-Gallery/src/test/java/services/RoomServiceTest.java
@@ -1,9 +1,12 @@
 
 package services;
 
+import java.util.Collection;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +15,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.Assert;
 
+import security.LoginService;
 import utilities.AbstractTest;
+import domain.Director;
 import domain.Museum;
 import domain.Room;
 
@@ -35,6 +40,9 @@ public class RoomServiceTest extends AbstractTest {
 
 	@Autowired
 	private MuseumService	museumService;
+
+	@Autowired
+	private DirectorService	directorService;
 
 
 	// -------------------------------------------------------------------------------
@@ -118,6 +126,89 @@ public class RoomServiceTest extends AbstractTest {
 
 			//3. Furthermore, the product must not be in the system
 			Assert.isNull(this.roomService.findOne(room.getId()));
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.unauthenticate();
+		this.checkExceptions(expected, caught);
+
+	}
+
+	/*
+	 * [UC-030] - Director create a room
+	 * 1. Log in as director
+	 * 2. List my rooms
+	 * 3. Create a room
+	 * 
+	 * REQ: 4
+	 * 
+	 * v1.0 - josembell
+	 */
+	@Test
+	public void driverCreateRoom() {
+
+		final Object testingData[][] = {
+			{
+				/* + 1. Un director crea un room para un museo suyo */
+				"director1", "museum1", "TEST", 10.50, null
+			}, {
+				/* - 2. Un usuario no identificado crea un room */
+				null, "museum1", "TEST", 10.50, IllegalArgumentException.class
+			}, {
+				/* - 3. Un usuario que no es director crea un room */
+				"visitor1", "museum1", "TEST", 10.50, IllegalArgumentException.class
+			}, {
+				/* - 4. Un director crea un room para un museum que no es suyo */
+				"director1", "museum7", "TEST", 10.50, IllegalArgumentException.class
+			}, {
+				/* - 5. Un director crea un room para un museum null */
+				"director1", null, "TEST", 10.50, IllegalArgumentException.class
+			}, {
+				/* - 6. Un director crea un room sin nombre */
+				"director1", "museum1", null, 10.50, ConstraintViolationException.class
+			}, {
+				/* - 7. Un director crea una room con area invalida */
+				"director1", "museum1", "TEST", 0.00, ConstraintViolationException.class
+			}, {
+				/* - 8. Un director crea una room sin area */
+				"director1", "museum1", "TEST", null, ConstraintViolationException.class
+			}
+
+		};
+
+		for (int i = 0; i < testingData.length; i++) {
+			Museum museum = null;
+			if (testingData[i][1] != null)
+				museum = this.museumService.findOne(this.getEntityId((String) testingData[i][1]));
+			this.startTransaction();
+			this.templateCreateRoom((String) testingData[i][0], museum, (String) testingData[i][2], (Double) testingData[i][3], (Class<?>) testingData[i][4]);
+			this.rollbackTransaction();
+			this.entityManager.clear();
+		}
+
+	}
+
+	/* v1.0 - josembell */
+	private void templateCreateRoom(final String username, final Museum museum, final String name, final Double area, final Class<?> expected) {
+		Class<?> caught = null;
+		/* 1. authenticate */
+		this.authenticate(username);
+
+		try {
+			/* 3. create room */
+			final Room room = this.roomService.create();
+			room.setArea(area);
+			room.setName(name);
+			room.setMuseum(museum);
+
+			final Room saved = this.roomService.saveCreateAndEdit(room);
+
+			/* 2. list my rooms */
+			final Director director = this.directorService.findByUserAccount(LoginService.getPrincipal());
+			final Collection<Room> myRooms = this.roomService.getByDirector(director);
+			Assert.isTrue(myRooms.contains(saved));
 
 		} catch (final Throwable oops) {
 			caught = oops.getClass();

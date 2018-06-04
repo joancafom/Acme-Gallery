cd306f712ecda823cb4d090e7c0d89453c4cdce9
joancafom <joanca.carmona597@gmail.com>
2018-06-04 16:03:26 +0200

    Perf Creation Script Fixed 🍓

--- Item 6 - Performance Test Suite/Create-Acme-Gallery.sql
+++ Item 6 - Performance Test Suite/Create-Acme-Gallery.sql
@@ -1,21 +1,21 @@
 ﻿start transaction;
 
-drop database if exists `Acme-Newspaper`;
-create database `Acme-Newspaper`;
+drop database if exists `Acme-Gallery`;
+create database `Acme-Gallery`;
 
-use `Acme-Newspaper`;
+use `Acme-Gallery`;
 
 create user 'acme-user'@'%' identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
 
 create user 'acme-manager'@'%' identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
 
 grant select, insert, update, delete 
-  on `Acme-Newspaper`.* to 'acme-user'@'%';
+  on `Acme-Gallery`.* to 'acme-user'@'%';
 
 grant select, insert, update, delete, create, drop, references, index, alter, 
         create temporary tables, lock tables, create view, create routine, 
         alter routine, execute, trigger, show view
-    on `Acme-Newspaper`.* to 'acme-manager'@'%';
+    on `Acme-Gallery`.* to 'acme-manager'@'%';
 
 -- MySQL dump 10.13  Distrib 5.5.29, for Win64 (x86)
 --
